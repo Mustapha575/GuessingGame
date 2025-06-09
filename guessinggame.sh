01105c9efdf2da3be888fd2753d6985af221af0f
Mustapha575 <mustaphag575@gmail.com>
2025-06-08 18:58:04 -0700

    Update README and guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -6,16 +6,21 @@ count=0
 while [ $count -lt 5 ]; do 
     echo "You have $count/5 chances to guess the number." 
     read -p "Guess a number between 1 and 10: " guess
+    if [ -z "$guess" ]; then
+        echo "You didn't pick a number!" 
+        exit 1
+    fi
     if [ $target -eq $guess ]; then 
         echo "Congrats! You guessed the number $target correctly!"
         break
     elif [ $target -gt $guess ]; then 
-        echo "Sucks for you! $guess is too low!" 
+        echo "Darn! $guess is too low!" 
     elif [ $target -lt $guess ]; then 
-        echo "Sucks for you! $guess is too high!"
-    elif [ -z "$guess" ]; then
-        echo "You didn't pick a number!" 
-        exit 1
+        echo "Darn! $guess is too high!"
+    fi
+    if [ $count -eq 5 ]; then
+        echo "Sorry, you're out of guesses. The number was $target."
+        exit 0
     fi
     ((count++))
 done 
